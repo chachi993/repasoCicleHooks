437e186b503e32fe47d38ebd086908e5c421f3f7
lina.beltran <lina.beltran@pyxisportal.co>
2022-03-28 16:42:47 -0500

    some change on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,55 @@
-import { Component, QueryList, ViewChildren } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   isChild = false;
-  firstName = "";
+  firstName = '';
 
-  @ViewChildren("div")
-  pepitoPerez!: QueryList<any>;
+  /*ViewChildren don’t include elements that exist within the ng-content tag.*/
+  @ViewChildren('div') pepitoPerez!: QueryList<any>;
 
-  @ViewChildren(ChildComponent)
-  juanita!: QueryList<ChildComponent>;
+  @ViewChildren(ChildComponent, { read: ElementRef })
+  juanita!: QueryList<ElementRef>;
+
+  /*ContentChildren includes only elements that exists within the ng-content tag.*/
+  //@ContentChildren()
 
   constructor() {
-    console.log("Parent Constructor is called");
-   }
+    console.log('Parent Constructor is called');
+  }
 
   ngOnInit(): void {
-    console.log("Parent OnInit is called");
-    console.log("OnInit "+ this.firstName)
-
+    console.log('Parent OnInit is called');
+    console.log('OnInit ' + this.firstName);
   }
 
   ngOnChanges() {
-    console.log("Parent Onchanges");
-    console.log("Onchanges "+ this.firstName)
-
+    console.log('Parent Onchanges');
+    console.log('Onchanges ' + this.firstName);
   }
 
   ngDoCheck() {
-    console.log("Parent DoCheck");
-    console.log("DoCheck "+this.firstName)
+    console.log('Parent DoCheck');
+    console.log('DoCheck ' + this.firstName);
   }
 
+  /*The QueryList is initialized only before the ngAfterViewInit lifecycle hook,
+  therefore, is available only from this point. */
   ngAfterViewInit() {
     const myFirst = this.pepitoPerez.changes;
-    this.pepitoPerez.forEach((divInstance:any) => console.log(divInstance));
-    this.juanita.forEach(childInstance => console.log(childInstance));
+    this.pepitoPerez.forEach((divInstance: any) => console.log(divInstance));
+    this.juanita.forEach((childInstance) => console.log(childInstance));
+    console.log(this.pepitoPerez.length);
+    console.log(this.pepitoPerez.toArray());
     console.log(myFirst);
   }
 
-  toggleChild(){
+  toggleChild() {
     console.log(this.isChild);
     this.isChild = !this.isChild;
     console.log(this.isChild);
